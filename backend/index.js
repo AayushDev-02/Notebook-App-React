bc51370b4bb291830581fde9824d3a696abaf60b
Aayush Yadav <yadavaayush002@gmail.com>
2023-02-22 10:25:02 +0530

    Made modals and schemas

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Aayush!')
-})
+//Available Routers
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
